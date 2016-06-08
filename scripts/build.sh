552d81627df0b0dfa8978b981d98443f220ad3de
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-06-08 17:31:14 +0300

    moved assets to volumes (drop assets image)

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ echo "--> using composer to build the application"
 cd ${PROJECT_SOURCE} && wundertools composer --pwd update
 
 echo "--> Building source image"
-docker build --tag="${BUILD_IMAGE_ROOT}-source:${BUILD_VERSION}" "${PATH_SOURCE}"
+docker build --tag="${BUILD_IMAGE_ROOT}-source:${IMAGE_VERSION}" "${PATH_SOURCE}"
 
 echo "--> Building assets image"
-docker build --tag="${BUILD_IMAGE_ROOT}-assets:${BUILD_VERSION}" "${PATH_ASSETS}"
+docker build --tag="${BUILD_IMAGE_ROOT}-assets:${IMAGE_VERSION}" "${PATH_ASSETS}"
