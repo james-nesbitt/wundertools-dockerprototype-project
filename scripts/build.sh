bbc6f0a5faa1a53a3e3726525ec879b5fcf69728
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-06-20 17:27:03 +0300

    updated for latest image approach

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,10 @@ echo "--> using composer to build the application [${PATH_SOURCE}]"
 #cd "${PATH_SOURCE}" && wundertools composer update
 
 echo "--> Building source image"
-docker build --tag="${BUILD_IMAGE_ROOT}-source:${IMAGE_VERSION}" "${PATH_SOURCE}"
+echo docker build --tag="${BUILD_IMAGE_ROOT}-source:${IMAGE_VERSION}" "${PATH_SOURCE}"
+
+echo "--> Building Database image"
+docker build --tag="${BUILD_IMAGE_ROOT}-data:${IMAGE_VERSION}" "${PATH_DATA}"
 
 echo "--> Building assets image"
-docker build --tag="${BUILD_IMAGE_ROOT}-assets:${IMAGE_VERSION}" "${PATH_ASSETS}"
+echo docker build --tag="${BUILD_IMAGE_ROOT}-assets:${IMAGE_VERSION}" "${PATH_ASSETS}"
